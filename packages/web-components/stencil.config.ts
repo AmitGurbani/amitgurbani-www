1af051f63163540b8c1dde335e02a02cd26d6562
Amit Gurbani <amit1994.gurbani@gmail.com>
2023-11-08 20:01:29 +0530

    Added tailwindcss

--- packages/web-components/stencil.config.ts
+++ packages/web-components/stencil.config.ts
@@ -1,4 +1,16 @@
 import { Config } from '@stencil/core';
+import tailwind, { tailwindHMR, setPluginConfigurationDefaults, TailwindPluginConfig } from 'stencil-tailwind-plugin';
+import tailwindcss from 'tailwindcss';
+import tailwindConf from './tailwind.config';
+import autoprefixer from 'autoprefixer';
+
+setPluginConfigurationDefaults({
+  tailwindConf: tailwindConf as TailwindPluginConfig,
+  tailwindCssPath: './src/styles/tailwind.css',
+  postcss: {
+    plugins: [tailwindcss(), autoprefixer()],
+  },
+});
 
 export const config: Config = {
   namespace: 'web-components',
@@ -18,7 +30,8 @@ export const config: Config = {
       serviceWorker: null, // disable service workers
     },
   ],
+  plugins: [tailwind(), tailwindHMR()],
   testing: {
-    browserHeadless: "new",
+    browserHeadless: 'new',
   },
 };
